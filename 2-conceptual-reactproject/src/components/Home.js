b312183b3d66ab8e21658c7f9fbaa3fcf2a4e995
Shamdasani, Rahul <rshamdas@iu.edu>
2022-01-01 23:40:49 -0500

    Prop Drilling layout

--- 2-conceptual-reactproject/src/components/Home.js
+++ 2-conceptual-reactproject/src/components/Home.js
@@ -1,71 +1,7 @@
 import React from 'react';
 import HomeCard from './HomeCard';
+import { pages } from './pageData';
 const Home = () => {
-	const pages = [
-		{
-			title: 'Mapping',
-			description:
-				'This is a simple example of mapping i.e. using a loop to iterate over an array and display the data in a card',
-			moreDetails: '/Mapping'
-		},
-		{
-			title: 'Title Change',
-			description:
-				'This is a simple example of using useState to change the title of the card when the button is clicked, and also using useEffect ',
-			moreDetails: '/usestate'
-		},
-		{
-			title: 'Array Change',
-			description:
-				'This is a simple example of using useState to manipulate an array and display the data in a card',
-			moreDetails: '/usestate2'
-		},
-		{
-			title: 'Counter',
-			description:
-				'This is a simple example of using useState to keep counter and display the data, <code>useEffect</code> is used to update the counter, one complex operation was update the counter after a delay',
-			moreDetails: '/counter'
-		},
-		{
-			title: 'useEffect',
-			description:
-				'This is a example of using useEffect to update the title of the HTML Page when the counter is changed',
-			moreDetails: '/useeffect'
-		},
-		{
-			title: 'fetchData',
-			description: 'We will use JSON placeholder api to fetch posts and display them in a card',
-			moreDetails: '/fetchdata'
-		},
-		{
-			title: 'conditional Rending',
-			description:
-				'In conditional rendering we will again practice fetching data from github users api and will return data if it is fetched else we will return loading',
-			moreDetails: '/conditionalrendering'
-		},
-		{
-			title: 'PhoneBook Forms and controlled inputs',
-			description:
-				'In this section we will create a phonebook where user can create the form, and the input data is displayed in a card',
-			moreDetails: '/phonebook'
-		},
-		{
-			title: 'Large forms and setting single function to handle multiple events',
-			description:
-				'In this section we will see how to handle multiple events in a form and how to set a single function to handle multiple events',
-			moreDetails: '/largeform'
-		},
-		{
-			title: 'useRef, which means uncontrolled inputs',
-			description: 'In this section we will see how we can use useRef to create a controlled input.',
-			moreDetails: '/useRefUseCase'
-		},
-		{
-			title: 'useReducer to manage model and state',
-			description: 'We will see how to use useReducer to manage model and state',
-			moreDetails: '/useReducer'
-		}
-	];
 	return (
 		<div className="container" style={{ width: '70vw' }}>
 			<div className="">
--- 2-conceptual-reactproject/src/components/Layout.js
+++ 2-conceptual-reactproject/src/components/Layout.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Layout = () => {
+	const [ dropdown, setDropdown ] = React.useState(false);
+
+	const toggleMenu = () => {
+		setDropdown(!dropdown);
+		var dropdowns = document.getElementsByClassName('dropdown-content');
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	};
+
 	return (
 		<div className="menu">
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -8,60 +23,67 @@ const Layout = () => {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
 						<li className="nav-item">
-							<a className="nav-link" href="/Mapping">
-								Mapping
+							<a className="nav-link " href="/">
+								Home
 							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/usestate">
-								Title Change
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/usestate2">
-								ArrayChange-UseState
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/counter">
-								Counter
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/useeffect">
-								useEffect
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/fetchdata">
-								FetchData
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link " href="/conditionalRending">
-								conditionalRending
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link " href="/phonebook">
-								PhoneBook
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link " href="/largeForm">
-								LargeForm
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link " href="/useRefUseCase">
-								UseRef/UncontrolledINputs
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link " href="/useReducer">
-								useReducer
-							</a>
-						</li>
+
+						<div className="dropdown">
+							<button
+								className="btn btn-secondary dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+								onClick={toggleMenu}
+							>
+								Dropdown button
+							</button>
+							{dropdown && (
+								<div className="">
+									<a className="dropdown-item show" href="/Mapping">
+										Mapping
+									</a>
+									<a className="dropdown-item show" href="/usestate">
+										Title Change
+									</a>
+									<a className="dropdown-item show" href="/usestate2">
+										ArrayChange-UseState
+									</a>
+									<a className="dropdown-item show" href="/counter">
+										Counter
+									</a>
+									<a className="dropdown-item show" href="/useeffect">
+										useEffect
+									</a>
+									<a className="dropdown-item show" href="/fetchdata">
+										FetchData
+									</a>
+									<a className="dropdown-item  show" href="/conditionalRending">
+										conditionalRending
+									</a>
+									<a className="dropdown-item  show" href="/phonebook">
+										PhoneBook
+									</a>
+									<a className="dropdown-item show" href="/largeForm">
+										LargeForm
+									</a>
+									<a className="dropdown-item show" href="/useRefUseCase">
+										UseRef/UncontrolledINputs
+									</a>
+									<a className="dropdown-item show" href="/useReducer">
+										useReducer
+									</a>
+									<a className="dropdown-item show" href="/propDrilling">
+										PropDrilling
+									</a>
+									<a className="dropdown-item show" href="/useContext">
+										UseContext
+									</a>
+								</div>
+							)}
+						</div>
 					</ul>
 				</div>
 			</nav>
