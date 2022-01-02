7468c95b4a0139099057eeec7c8c445e1a061371
Shamdasani, Rahul <rshamdas@iu.edu>
2022-01-01 23:41:15 -0500

    useContext

---
+++ 2-conceptual-reactproject/src/components/UseContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UseContext = () => {
+	return (
+		<div className="container">
+			<h4>UseContext</h4>
+		</div>
+	);
+};
+
+export default UseContext;
--- 2-conceptual-reactproject/src/index.js
+++ 2-conceptual-reactproject/src/index.js
@@ -10,6 +10,7 @@ import FetchData from './components/FetchData';
 import ConditionalRendering from './components/ConditionalRendering';
 import UseReducer from './components/UseReducer/index';
 import UseReducer2 from './components/useReducer2/index';
+import PropDrilling from './components/PropDrilling';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // css
@@ -18,6 +19,7 @@ import Counter from './components/Counter';
 import PhoneBook from './components/PhoneBook';
 import LargeForm from './components/LargeForm';
 import UseRefUseCase from './components/UseRefUseCase';
+import UseContext from './components/UseContext';
 
 function Index() {
 	return (
@@ -37,6 +39,8 @@ function Index() {
 					<Route exact path="/largeform" element={<LargeForm />} />
 					<Route exact path="/useRefUseCase" element={<UseRefUseCase />} />
 					<Route exact path="/useReducer" element={<UseReducer2 />} />
+					<Route exact path="/propDrilling" element={<PropDrilling />} />
+					<Route exact path="/useContext" element={<UseContext />} />
 					<Route path="*" element={<Home />} />
 				</Routes>
 			</Router>
