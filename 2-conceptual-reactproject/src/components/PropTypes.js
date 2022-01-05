f0af4e64c6ad6c484ad1e204ef6b34ebc22a70d3
Shamdasani, Rahul <rshamdas@iu.edu>
2022-01-04 23:17:38 -0500

    Prop Types component

--- 2-conceptual-reactproject/src/components/PropTypes.js
+++ 2-conceptual-reactproject/src/components/PropTypes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { data } from './PropTypesData';
+import PropTypesDetails from './PropTypesDetails';
 
 const PropTypes = () => {
 	return (
 		<div className="container">
 			<h4>Prop Types example</h4>
+			<div className="item-list">
+				{data.map((item, index) => {
+					return <PropTypesDetails key={index} {...item} />;
+				})}
+			</div>
 		</div>
 	);
 };
