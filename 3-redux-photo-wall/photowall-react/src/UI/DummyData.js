ad7fe099e99ff16152f39203ef47d5ebe92a14f9
Shamdasani, Rahul <rshamdas@iu.edu>
2022-01-12 23:42:40 -0500

    dummy data

---
+++ 3-redux-photo-wall/photowall-react/src/UI/DummyData.js
@@ -0,0 +1,30 @@
+export const data = [
+	{
+		id: 1,
+		url:
+			'https://media-exp1.licdn.com/dms/image/C5103AQEU1h3_qqqQWg/profile-displayphoto-shrink_800_800/0/1535639999519?e=1647475200&v=beta&t=U-0IrO1FO4EIHuBERMjiftoAcqcV_PBkVNMNyeMvLV8',
+		description:
+			'I am a full stack developer with a passion for learning and building things that make a difference.'
+	},
+	{
+		id: 2,
+		url:
+			'https://media-exp1.licdn.com/dms/image/C5103AQEU1h3_qqqQWg/profile-displayphoto-shrink_800_800/0/1535639999519?e=1647475200&v=beta&t=U-0IrO1FO4EIHuBERMjiftoAcqcV_PBkVNMNyeMvLV8',
+		description:
+			'I am a full stack developer with a passion for learning and building things that make a difference.'
+	},
+	{
+		id: 3,
+		url:
+			'https://media-exp1.licdn.com/dms/image/C5103AQEU1h3_qqqQWg/profile-displayphoto-shrink_800_800/0/1535639999519?e=1647475200&v=beta&t=U-0IrO1FO4EIHuBERMjiftoAcqcV_PBkVNMNyeMvLV8',
+		description:
+			'I am a full stack developer with a passion for learning and building things that make a difference.'
+	},
+	{
+		id: 4,
+		url:
+			'https://media-exp1.licdn.com/dms/image/C5103AQEU1h3_qqqQWg/profile-displayphoto-shrink_800_800/0/1535639999519?e=1647475200&v=beta&t=U-0IrO1FO4EIHuBERMjiftoAcqcV_PBkVNMNyeMvLV8',
+		description:
+			'I am a full stack developer with a passion for learning and building things that make a difference.'
+	}
+];
